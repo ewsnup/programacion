89d0a42a78cbe2d788fa0c520b0f3122e3f15b2a
Ludmila <awlumii505@gmail.com>
2025-05-12 23:45:44 -0300

    Update Ejercicio7.py

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -5,18 +5,3 @@ from Biblioteca import *
 mostrar_menu()
 opcion_menu()
 
-
-
-
-
-
-
-
-
-'''
-Listar los datos de los usuarios de México y Brasil cuyo código postal
-sea mayor a 8000 ordenado por nombre y edad de manera descendente
-
-
-'''
-
